f9a7d75bff293b18be3b3d712bea4f3b47fc7b4b
Joshua <josh.joshua1@gmail.com>
2018-08-23 00:11:59 +0530

    Worked on Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {RouterModule, Routes} from '@angular/router';
+import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  {path:'',component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   path:any = 'http://www.pngnames.com/files/1/Josh-Name-Free-PNG-Logo.png';
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
